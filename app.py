941c519353518efd1dd3498ba3f43632efa287af
Min Kwon <mkwon1203@gmail.com>
2016-01-23 06:37:30 -0700

    added signIn method, added correct sql query for nearbyBookstore

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask
+from flask import jsonify
 import MySQLdb
 
 
@@ -22,18 +23,41 @@ def nearbyBookstore( ):
 	cursor = db.cursor()
 
 	# execute SQL query using execute() method.
-	cursor.execute("SELECT VERSION()")
+	cursor.execute("Select ISBN from BookCourse where CID in ( SELECT CID from Enroll where SID = 4)")
 
 	# Fetch a single row using fetchone() method.
-	data = cursor.fetchone()
+	data = cursor.fetchall()
+	
+	for row in data:
+		print row
 
-	print "Database version : %s " % data
 
 	# disconnect from server
 	db.close()
-	return "Database version : %s " % data
+	return jsonify(data)
 
+# put in SID as param later
+@app.route('/classroom/attendance/signin')
+def signIn():
+	#check if they've picked up textbooks
+	#if not, respond to notify to grab them
+	# Open database connection
+	db = MySQLdb.connect("localhost","root","D0nkeyba!!s","mobievent" )
 
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	# execute SQL query using execute() method.
+	cursor.execute("use mobievent")
+	cursor.execute("INSERT INTO Attendance values (372, 1, CURRENT_TIMESTAMP, True")
+
+	print "attendance input successful"
+
+	# disconnect from server
+	db.close()
+	return 0
+	
+	
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
