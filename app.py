47728e99b6cab3962169cf47f6068f5b2f0a2231
Jarryd Goodman <jarryd999@gmail.com>
2016-01-23 05:25:31 -0700

    Testing Mysql
    trying mysqldb

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 #!flask/bin/python
 from flask import Flask
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+# disconnect from server
+db.close()
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return "Hello, World!"
+
+#handle walking by the store
+#http://ourserver.cloud.google.com/bookstore/nearby
+@app.route('/bookstore/nearby')
+def nearbyBookstore( SID ):
+	#check if they've picked up textbooks
+	#if not, respond to notify to grab them
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
