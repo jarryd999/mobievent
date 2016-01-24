36d7deb415b1999ff2ff22679e8418a14796d309
Min Kwon <mkwon1203@gmail.com>
2016-01-24 08:15:38 -0700

    t

--- app.py
+++ app.py
@@ -235,11 +235,11 @@ def paint(region, centerX, centerY, radius):
 	if region == None:
 		return
 	offset = radius * math.cos(math.radians(45))
-	if (contains(region, centerX, centerY) or contains(region, centerX + offset, centerY + offset) 
-	or contains(region, centerX + offset, centerY - offset) or contains(region, centerX - offset, centerY + offset)
-	or contains(region, centerX - offset, centerY - offset) or contains(region, centerX + radius, centerY)
-	or contains(region, centerX - radius, centerY) or contains(region, centerX, centerY + radius)
-	or contains(region, centerX, centerY - radius)):
+	if (!contains(region, centerX, centerY) and !contains(region, centerX + offset, centerY + offset) 
+	and !contains(region, centerX + offset, centerY - offset) and !contains(region, centerX - offset, centerY + offset)
+	and !contains(region, centerX - offset, centerY - offset) and !contains(region, centerX + radius, centerY)
+	and !contains(region, centerX - radius, centerY) and !contains(region, centerX, centerY + radius)
+	and !contains(region, centerX, centerY - radius)):
 		return
 	distanceTop = sqrt( (region.topX - centerX)**2 + (region.topY - centerY)**2)
 	distanceBot = sqrt( (region.botX - centerX)**2 + (region.botY - centerY)**2)
